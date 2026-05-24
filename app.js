aa06a9c0e48cd121fb83ccec7947f2d6daddba5b
Hamza Hussain <hunterhomi4@gmail.com>
2026-05-24 11:02:33 -0700

    Feat: Complete Phase 1 Core Task Engine with working EJS loop and POST route

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ import bodyParser from "body-parser";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { get } from "http";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+let taskList = [];
 
 app.set("view engine", "ejs");
 
@@ -14,9 +16,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index"); 
+    res.render("index", { tasks: taskList });
 })
 
 app.listen(port, () => {
     console.log(`The server is running on port ${port}.`);
 })
+
+app.post("/submit-task", (req, res) => {
+    taskList.push(req.body.newTask);
+    res.redirect("/");
+});
